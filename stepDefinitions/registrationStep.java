2e0184f9a85c20153cef43e87fcb719f106361b6
TundeAsiyanb <bab5_4@hotmail.com>
2024-03-05 17:36:03 +0000

    Initial commit
    test2

--- stepDefinitions/registrationStep.java
+++ stepDefinitions/registrationStep.java
@@ -24,6 +24,7 @@ public class registrationStep {
         registrationPage = new RegistrationPage(driver);
         registrationPage.OpenUrl();
         // test12123
+        //testing125
     }
 
     @And("i enter my first name {string}")
